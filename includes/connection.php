1c8744ba325202d9685382ac25eeadb12ad0cf22
Oliver Crowe <olivercampbellcrowe@outlook.com>
2019-12-04 09:59:40 +0000

    Front end index

--- includes/connection.php
+++ includes/connection.php
@@ -1,3 +1,3 @@
 <?php
-
+    $conn = mysqli_query("localhost", "root", "", "anonmessaging");
 ?>
--- index.php
+++ index.php
@@ -8,7 +8,41 @@
     <link href="css/style.css" rel="stylesheet">
 </head>
 <body>
-    <div style="height:3000px;"></div>
-    
-</body>
+<div class="main-hold">
+    <div class="first-col col">
+        <div class="hero-box">
+            <h1>AnonMessaging</h1>
+            <p>An anonymous messaging site</p>
+        </div>
+        <div class="features-box">
+            <h3>Features</h3>
+            <ul>
+                <li>Full anonymity</li>
+                <li>Group chats</li>
+                <li>Private messaging</li>
+                <li>Large shoutout groupchat, all can enter</li>
+                <li>Full power to <u>permanently</u> delete all messages</li>
+            </ul>
+        </div>
+        <div class="extra">
+        <h3>Some extra info</h3>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi deserunt recusandae provident numquam ullam, molestiae, ipsam nam dolore sit possimus enim. Placeat aliquid quo consequuntur quam? Autem odio porro facilis!</p>
+        </div>
+    </div>
+    <div class="second-col col">
+    <div class="login-form">
+        <h2 class="form-title">Sign up</h2>
+        <form method="POST">
+        <label for="user_email">Email</label>
+        <input type="email" name="user_email" id="user_email" placeholder="anon@messaging.com"  required>
+        <label for="user_name">Name</label>
+        <input type="text" name="user_name" id="user_name" placeholder="John Doe" required>
+        <label for="user_password">Password</label>
+        <input type="password" name="user_password" id="user_password" placeholder="***********" required>
+        <button type="submit" name="submit" class="submit-btn">Signup</button>
+        </form>
+    </div>
+    </div>
+</div>
+</div>
 </html>
