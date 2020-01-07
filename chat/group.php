05990bc575bc2092112f17f43fde3e3a3d818fee
Oliver Crowe <olivercampbellcrowe@outlook.com>
2020-01-07 14:17:03 +0000

    Font end group chat

---
+++ chat/group.php
@@ -0,0 +1,68 @@
+<?php
+    session_start();
+    if(!isset($_SESSION['user_id'])){
+        header("../login");
+    }
+    include_once "../includes/connection.php";
+    include_once "../includes/functions.php";
+    if(!isset($_GET['id'])){
+        echo "<script>window.location = '../index.php'</script>";
+        exit();
+    }
+    
+    checkBan($_SESSION['user_id']);
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>AnonMessaging - Fully anonymous messaging site</title>
+    <link href="../css/style.css" rel="stylesheet">
+</head>
+<body>
+    <div class="main-column">
+    <a style="color: #000;text-align:center;" href="../"><h1 style="font-weight:100;margin-bottom:20px;">AnonMessaging</h1></a>
+        <h1 style="font-weight:300;">Conversation with: 
+        <!-- <u><a style="color: #000;" href="../profile/index.php?id=<?php echo $name; ?>"><?php echo findName($name); ?></a></u> -->
+        </h1>
+        <br><hr><br>
+        <form method="POST">
+            <textarea name="msg_content" class="msg_textarea" required placeholder="Enter your message"></textarea>
+            <button type="submit" name="submit" class="form-submit">Send</button>
+        </form>
+        
+        <div class="message">
+            <div class="msg-info">
+                
+            <a style="color:#000;" href="../profile/index.php?id=ID"><h3 style="font-weight:300;">NAME</h3></a>
+            <p>DATE</p>
+            <p>User Points: test</p>
+            
+            </div>
+            
+            <div class="msg-content"?>
+           ashffhshfd
+            </div>
+        </div>
+          
+
+        </div>
+        <div class="right-column">
+            <h2 style="font-weight:300;">Actions</h2>
+            <hr>
+            <ul>
+            <li><a href="../">Dashboard</a></li>
+                <li><a href="../profile/index.php?id=<?php echo $_SESSION['user_id']; ?>">Profile</a></li>
+                <li><a href="../logout">Logout</a></li>
+                <li><a href="../users/index.php">Users</a></li>
+                <li><a href="../leaderboard/">Leaderboard</a></li>
+                <li><a href="../global/">Global chat</a></li>
+                <li><a href="../suggestions/">Suggestions</a></li>
+            </ul>
+        </div>
+    </div>
+</body>
+</html>
